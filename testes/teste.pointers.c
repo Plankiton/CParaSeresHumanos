6434d505fe11fcaea79c33f6183a15f0582986c9
RoboCopGay <vinicios.sousa909@gmail.com>
2019-07-29 20:29:35 -0300

    ...

--- testes/teste.pointers.c
+++ testes/teste.pointers.c
@@ -3,14 +3,11 @@
 int main(int argc, char **argv){
 
    char *list[] = {"joao", "maria", "boiolage"};
-   char * aux;
 
    int i = 0;
    printf("[ ");
    while ( i < 3){
-      aux = list[i];
-      printf(" \"%s\",", aux);
-      i ++;
+      printf(" \"%s\",", list[i]);
    }
    printf("\b ]");
 
