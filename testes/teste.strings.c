a90939bb6116cf6241af853d1b5591c25dda942a
RoboCopGay <vinicios.sousa909@gmail.com>
2019-07-29 19:09:49 -0300

    add info strings

--- testes/teste.strings.c
+++ testes/teste.strings.c
@@ -3,6 +3,8 @@
 #include <string.h>
 int main(){
    /*
+
+   // ponteiros e strings
    char j [20];
    char * str = &j[0];
    while (strlen(j) < 20){
@@ -12,15 +14,7 @@ int main(){
    * str = '\0';
    printf("%s", j);
 
-   // ...
-
-   char str [] = "joao";
-   char j = str[0];
-
-   printf("%c\n", j);
-   */
-
-   // ... ++
+   // strings e strings
 
    char str [] = "jnjnj";
    char k[] = "o joao maria eu";
@@ -29,5 +23,12 @@ int main(){
    printf("%s\n"
          "%s\n"
          "%s", j, k, str);
+   */
+
+   // atribuindo strings a strings sem o string.h
+   char * string;
+
+   string = "joao";
+   puts(string);
 return 0;
 }
