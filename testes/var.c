f1f26ff1c621df8b3d49bf57eb6e2c7424f4ce9e
RoboCopGay <vinicios.sousa909@gmail.com>
2020-04-21 14:14:11 -0300

    testando de novo

--- testes/var.c
+++ testes/var.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char **argv){
-   int n = 666, m = n, s = m;
+    int n, m, s;
+    n = m = s = 666;
+
    printf ("%i %i %i\n", n, m, s);
+
+   float f = 1 / 3;
+
+   printf ("%f\n", f);
+   printf ("%f\n", (f = 1.0 / 3.0));
 return 0;
 }
